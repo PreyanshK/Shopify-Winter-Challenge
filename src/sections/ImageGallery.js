7c1e43d351429fb9a80e05afa845388b64fbe3b2
Preyansh <preyanshkachhiapk@gmail.com>
2021-09-15 10:59:37 -0400

    Added loading indicator and fixed photoData state

--- src/sections/ImageGallery.js
+++ src/sections/ImageGallery.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from '../components/ImageCard';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import '../styles/ImageGallery.css';
 
 const apiKey = process.env.REACT_APP_NASA_KEY;
 
 const ImageGallery = ({ startDate }) => {
-  const [photoData, setPhotoData] = useState(null);
+  const [photoData, setPhotoData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchNASAImages = async () => {
+    setLoading(true);
+
     const response = await fetch(
       `https://api.nasa.gov/planetary/apod?start_date=${startDate}&api_key=${apiKey}`
     );
 
     const data = await response.json();
 
+    setLoading(false);
     setPhotoData(data);
     console.log(data);
   };
@@ -25,15 +30,23 @@ const ImageGallery = ({ startDate }) => {
   //incase the API doesn't return any response
   if (!photoData) return <div />;
 
-  return (
-    <section className="imageGalleryContainer">
-      <div className="imagesContainer">
-        {photoData.map((image) => {
-          return <ImageCard image={image} />;
-        })}
+  if (loading) {
+    return (
+      <div className="loadingIndicator">
+        <CircularProgress color="inherit" />
       </div>
-    </section>
-  );
+    );
+  } else {
+    return (
+      <section className="imageGalleryContainer">
+        <div className="imagesContainer">
+          {photoData.map((image) => {
+            return <ImageCard image={image} />;
+          })}
+        </div>
+      </section>
+    );
+  }
 };
 
 export default ImageGallery;
